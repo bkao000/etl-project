9851e126ea54dcac1035b1e507b04817d38082fd
bkao000 <bkao000@gmail.com>
2023-07-22 20:14:41 -0700

    Collect rows with invalid Int, Date data

--- etl-app/exe/etl_order.py
+++ etl-app/exe/etl_order.py
@@ -1,124 +1,101 @@
 import csv
-#import yaml
 from ruamel.yaml import YAML
 from pathlib import Path
 import locale
 import datetime
-import transformation
-#import os
-#from lib.transformation.transformation import Transformation
-#import sys
-#print('sys.path = {0}'.format(sys.path))
-#print('cwd = {0}'.format(os.getcwd()))
-
-#sys.path.append('/Users/brendakao/my-app/crisp-lib')
-
-#data = yaml.load(open('./cfg/order.yaml'))
-#print(data['OrderID']['col'])
+from datetime import date
+import transformation           # custom built etl transformation library
 
 path = Path('./etl-app/cfg/order.yaml')
 yaml = YAML(typ='safe')
 mapping = yaml.load(path)['mapping']
-#print(type(mapping)) 
-#print(data)
-#print(mapping[0]['tgt'])
 
-'''
-with open('./cfg/order.yaml') as cfg_file:
-    docs = yaml.safe_load_all(cfg_file)
-    for doc in docs:
-        print(doc[0])
-cfg_file.close()    
-'''
-
-output_rst = []
-output_header_lst = []
-output_row_lst = []
-set_output_header = False
+output_header = []
+output_header_flag = False
+output_row_lst = []         # List to save output data of a row
+output_rst = []             # List to save output rows
+invalid_row_lst = []
+invalid_row_flag = False
 
 t = transformation.Transformation()
-# with open('/Users/brendakao/my-app/crisp-test/src/orders.csv') as order_file:
-with open('./etl-app/src/orders.csv') as order_file:
-#    print(os.getcwd())
-    reader = csv.DictReader(order_file)
 
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+with open('./etl-app/src/orders.csv') as order_file:
+    reader = csv.DictReader(order_file)
     
     for row in reader:
-        print(row)
+        #print(row)
         for i in range(len(mapping)):
-            #print(mapping[i]['tgt'])
-            #print(mapping[i]['type'])
-            #tgt_type = mapping[i]['type']
-            if  set_output_header is False:
-                output_header_lst.append(mapping[i]['tgt'])
+            # Get target data header once
+            if output_header_flag is False:
+                output_header.append(mapping[i]['tgt'])
+            
+            # Transform data based on target field definition in yaml file
             match mapping[i]['type']:
                 case 'Integer':
                     try:
-                        #tgt_int_value = int(row[mapping[i]['src']])
-                        tgt_int_value = t.StringToInt(row[mapping[i]['src']])
+                        tgt_int_value = t.StringToInteger(row[mapping[i]['src']])
+                        output_row_lst.append(tgt_int_value)
                     except ValueError:
                         tgt_int_value = None
-                    print('{0}: {1}     {2}'.format(mapping[i]['tgt'], tgt_int_value, mapping[i]['type']))
-                    output_row_lst.append(tgt_int_value)
+                    if tgt_int_value is None:
+                        invalid_row_flag = True# Invalid row needs to be captured
                 case 'BigDecimal':
                     try:
-                        #tgt_float_value = '%.2f' % locale.atof(row[mapping[i]['src']])
                         tgt_float_value = t.StringToFloat(row[mapping[i]['src']], 2)
+                        output_row_lst.append(tgt_float_value)
                     except ValueError:
                         tgt_float_value = None
-                    output_row_lst.append(tgt_float_value)
-                    print('{0}: {1}     {2}'.format(mapping[i]['tgt'], tgt_float_value, mapping[i]['type']))
+                        # Invalid row needs to be captured
                 case 'Date':
                     if mapping[i]['function'] == 'FormatDate':
-                        #date_string = '%s-%2s-%2s' % (row[mapping[i]['src'][0]], row[mapping[i]['src'][1]].zfill(2), row[mapping[i]['src'][2]].zfill(2)) 
-                        #print(date_string)
-                        #tgt_date_value = datetime.strptime(date_string, "%Y-%m-%d")
-                        #tgt_date_value = datetime.datetime(int(row[mapping[i]['src'][0]]), int(row[mapping[i]['src'][1]]), int(row[mapping[i]['src'][2]]))
                         tgt_date_value = t.FormatDate(row[mapping[i]['src'][0]], row[mapping[i]['src'][1]], row[mapping[i]['src'][2]])
-                        #print(tgt_date_value)
-                        print('{0}: {1}     {2}'.format(mapping[i]['tgt'], tgt_date_value, mapping[i]['type'])) 
                     else:
-                        print('{0}: {1}     {2}'.format(mapping[i]['tgt'], row[mapping[i]['src']], mapping[i]['type']))
-                    output_row_lst.append(tgt_date_value)
+                        tgt_date_value = None
+                    if tgt_date_value:
+                        output_row_lst.append(tgt_date_value)
+                    else:
+                        invalid_row_flag = True
                 case 'String':
-                    #print('{0}: {1}     {2}'.format(mapping[i]['tgt'], mapping[i]['src'], mapping[i]['type']))
                     if mapping[i]['src'] == 'n/a':
                         tgt_str_value = mapping[i]['default']
                     else:
                         tgt_str_value = row[mapping[i]['src']]
-                        print('1: tgt_str_value = {0}'.format(tgt_str_value))
                         try:
-                            if mapping[i]['case'] == 'TitleCase':
-                                tgt_str_value = tgt_str_value.title()    
-                                #print('case = |{0}|'.format(mapping[i]['case']))
-                            print('2: tgt_str_value = {0}'.format(tgt_str_value))
+                            match mapping[i]['case']:
+                                case 'TitleCase':
+                                    tgt_str_value = t.StringToTitleCase(tgt_str_value)
+                                case 'LowerCase':
+                                    tgt_str_value = t.StringToLowerCase(tgt_str_value)
+                                case 'UpperCase':
+                                    tgt_str_value = t.StringToUpperCase(tgt_str_value)
+                                case _:
+                                    print('Undefined String Case Type: {0}'.format(mapping[i]['case']))
+                                    # Invalid row needs to be captured
                         except:
                             pass # print('case not defined')
-                    print('{0}: {1}     {2}'.format(mapping[i]['tgt'], tgt_str_value, mapping[i]['type']))
+                            # Invalid row needs to ba captured
                     output_row_lst.append(tgt_str_value)
                 case _:        
                         print('{0}: {1}     {2}'.format(mapping[i]['tgt'], mapping[i]['src'], mapping[i]['type'])) 
-        if set_output_header is False:
-            output_rst.append(output_header_lst)   
-            set_output_header = True
-        output_rst.append(output_row_lst)
-        output_row_lst = []
-    '''
-        (order_number, year, month, day, product_number, product_name, count, extra_col1, extra_col2, empty_col) = row
-        print('order_number = '+row['Order Number'])
-        print('year = '+row['Year'])
-        print('month = '+row['Month'])
-        print('day = '+row['Day'])
-        print('product_number = '+row['Product Number'])
-        print('product_name = '+row['Product Name'])
-        print('count = '+row['Count'])
-        print('unit = kg')
-    '''
+        if invalid_row_flag is False:
+            if output_header_flag is False:
+                output_rst.append(output_header)
+                output_header_flag = True
+            output_rst.append(output_row_lst)
+        else:
+            invalid_row_lst.append(row.values())
+            invalid_row_flag = False# append the current row transformed output to result data set
+        output_row_lst = []                 # clear the row buffer
+ 
+current_date = str(date.today())
 
-    order_file.close()
-    
-print(output_rst)
-with open('./etl-app/tgt/order.csv', 'w+') as output_file:
+# Write results to output file    
+with open('./etl-app/tgt/order_'+current_date+'.csv', 'w+') as output_file:
     write = csv.writer(output_file)
     write.writerows(output_rst)
+    
+with open('./etl-app/err/order-err_'+current_date+'.csv', 'w+') as bad_data_file:
+    write = csv.writer(bad_data_file)
+    write.writerow(reader.fieldnames)
+    write.writerows(invalid_row_lst)
+    
--- etl-lib/transformation/transformation.py
+++ etl-lib/transformation/transformation.py
@@ -1,19 +1,36 @@
 """ This library provides functions for data transformation """
 import locale
 import datetime
+from ruamel.yaml import YAML
 
 class Transformation:
         
     def about(self):
-        print("This Library provides functions for data transformation")
+        print("\n*** This Library provides functions for data transformation ***\n")
         
-    def StringToInt(self, s):
-        return int(s)
+    def StringToInteger(self, s):
+        try:
+            return int(s)
+        except:
+            return None
 
     def FormatDate(self, year, month, day):
-        return datetime.datetime(int(year), int(month), int(day))
+        try:
+            return datetime.datetime(int(year), int(month), int(day))
+        except:
+            return None
 
     def StringToFloat(self, s, decimal_places):
         locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
         format_str = '%.'+str(decimal_places)+'f'
         return ( format_str % locale.atof(s))
+    
+    def StringToTitleCase(self, s):
+        return s.title()
+    
+    def StringToLowerCase(self, s):
+        return s.lower()
+    
+    def StringToUpperCase(self, s):
+        return s.upper()
+        
